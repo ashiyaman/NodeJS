a7a1e3ae9c7e2ce7f82feb43b4b7cfb02dea5319
ashiyaman <ashiya1005@gmail.com>
2025-06-09 16:05:12 +0530

    feat: Introduction

---
+++ 3.First code/app.js
@@ -0,0 +1,15 @@
+var name = "Ashiya"
+
+var a = 5
+
+var b = 10
+
+console.log(name)
+
+console.log(a + b)
+
+//console.log(global) //in Node, global obj is global 
+                    //same like this in browsers
+                    //hence globalThis will refer to global obj even in browsers, node, all browsers, etc
+
+console.log(globalThis === global)        
